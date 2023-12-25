0b89ba0f1fcea6f58dd83334fe1d1fba94934077
Mateus-A-Carvalho <mateusabreu94@hotmail.com>
2023-12-25 10:28:03 -0300

    feat: add the correct element to add dark-mode with JS
    The functionality of change the :root pseudo-element didn't worked. I change the bodyEl to htmlEl and it worked.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,12 +2,13 @@ const sunEl = document.querySelector('#sun')
 const moonEl = document.querySelector('#moon');
 const bodyEl = document.querySelector('body');
 const divWrapEl = document.querySelector('#div-wrap');
+const htmlEl = document.querySelector('html');
 
 document.addEventListener("click", (e) => {
   if(e.target === sunEl) {
-    bodyEl.classList.toggle('dark-mode');
+    htmlEl.classList.toggle('dark-mode');
   } else if(e.target === moonEl) {
-    bodyEl.classList.toggle('dark-mode');
+    htmlEl.classList.toggle('dark-mode');
   }
 })
 
