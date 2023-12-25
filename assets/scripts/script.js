00d3567ed6dd29e07b0f52f6ed45ded534c59aa3
Mateus-A-Carvalho <mateusabreu94@hotmail.com>
2023-12-25 09:19:59 -0300

    feat: add if/statement to add or remove .dark-mode class of the body

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,3 +3,11 @@ const moonEl = document.querySelector('#moon');
 const bodyEl = document.querySelector('body');
 const divWrapEl = document.querySelector('#div-wrap');
 
+document.addEventListener("click", (e) => {
+  if(e.target === sunEl) {
+    bodyEl.classList.toggle('dark-mode');
+  } else if(e.target === moonEl) {
+    bodyEl.classList.toggle('dark-mode');
+  }
+})
+
