8c8479ae9ff5d0a65dab7fd4a88dc9eed141cb5e
Mateus-A-Carvalho <mateusabreu94@hotmail.com>
2023-12-26 20:58:51 -0300

    fix: change the structure and styles of project
    I was doing the project in a way completly wrong and hard. I changed the way to do this and structure of project to get easier.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,26 +4,4 @@ const bodyEl = document.querySelector('body');
 const divWrapEl = document.querySelector('#div-wrap');
 const htmlEl = document.querySelector('html');
 
-document.addEventListener("click", (e) => {
-  if(e.target === moonEl) {
-    htmlEl.classList.toggle('dark-mode');
-    // adding classes;
-    moonEl.classList.add('enable');
-    sunEl.classList.add('desable');
-    // removing classes;
-    moonEl.classList.remove('desable');
-    sunEl.classList.remove('enable');
-
-  } else if(e.target === sunEl) {
-    htmlEl.classList.toggle('dark-mode');
-    // adding classes;
-    sunEl.classList.add('enable');
-    moonEl.classList.add('desable');
-    // removing classes;
-    sunEl.classList.remove('desable');
-    moonEl.classList.remove('enable');
-
-  }
-
-})
 
