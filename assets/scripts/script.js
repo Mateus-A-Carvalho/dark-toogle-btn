ae784c07c7578b72fb6d02163ae461a5126ebd1f
Mateus-A-Carvalho <mateusabreu94@hotmail.com>
2023-12-27 15:14:03 -0300

    feat: add feature to save style in localStorage
    Style and setted animations of page.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,26 @@
-const sunEl = document.querySelector('#sun')
-const moonEl = document.querySelector('#moon');
 const bodyEl = document.querySelector('body');
-const divWrapEl = document.querySelector('#div-wrap');
-const htmlEl = document.querySelector('html');
+const inputEl = document.querySelector('#dark-mode');
+const labelEl = document.querySelector('#label')
+const circleEl = document.querySelector('.circle')
 
+inputEl.checked = JSON.parse(localStorage.getItem("mode"));
+updateTheme();
 
+inputEl.addEventListener("input", () => {
+  updateTheme();
+  updateLocalStorage();
+})
+
+function updateTheme() {
+  if(inputEl.checked) {
+    bodyEl.style.background = "rgba(15, 15, 15)";
+    circleEl.style.backgroundColor = "#2b2b2b"
+  } else {
+    bodyEl.style.background = "rgba(245, 245, 245)";
+    circleEl.style.backgroundColor = "#fffffa"
+  }
+}
+
+function updateLocalStorage() {
+  localStorage.setItem("mode", JSON.stringify(inputEl.checked))
+}
